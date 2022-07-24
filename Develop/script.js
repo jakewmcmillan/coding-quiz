e167ef37f2f08954b257dd58d23fbf25aa36b38f
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-24 19:01:14 -0400

    Adding timer function

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+var timerEl = document.getElementsByClassName('timer');
+
+function timer () {
+    var timeLeft = 75;
+    var timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            timerEl.textContent = '';
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+    }, 1000);
+}
