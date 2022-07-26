deef3033ebe864375094fede8c82332488da9a24
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-26 18:06:14 -0400

    Moving javascript variables

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,31 @@ var questionEl = document.querySelector(".question");
 var answersEl = document.querySelector(".answer-list");
 var choicesEl = document.querySelector(".answer");
 
+var quiz = [
+    {
+      question: "Which of the following is a programming language?",
+      choices: ["Boolean", "console.log", "Javascript", "Array"],
+    },
+    {
+      question: "What 2 primary values does a Boolean represent?",
+      choices: ["Top/Bottom", "Right/Left", "Even/Odd", "True/False"],
+    },
+    {
+      question: "What syntax do developers use to denote an array?",
+      choices: ["(x,y)", "[x,y]", "{x,y}", "None of the above"],
+    },
+    {
+      question: "Which of the following are Javascript data types?",
+      choices: ["String", "Boolean", "Objects", "All of the above"],
+    },
+    {
+      question: "What does console.log() do",
+      choices: ["Outputs a message to the web console", "Deletes a function", "Creates an element", "Creates a variable"],
+    },
+  ];
+  
+  console.log(quiz);
+
 function timer() {
   var sec = 75;
   var timer = setInterval(function () {
@@ -13,6 +38,8 @@ function timer() {
     }
   }, 1000);
 }
+
+timer();
 
 //Altering questions
 
@@ -36,34 +63,3 @@ function timer() {
     
     //navigate(1);
 //});
-
-timer();
-
-Object = {
-  question: "Question 1",
-  answer: "Choice 3",
-  choices: ["Choice 1", "Choice 2", "Choice 3", "Choice 4"],
-};
-
-var quiz = [
-  {
-    question: "Which of the following is a programming language?",
-    choices: ["Boolean", "console.log", "Javascript", "Array"],
-  },
-  {
-    question: "What 2 primary values does a Boolean represent?",
-    choices: ["Top/Bottom", "Right/Left", "Even/Odd", "True/False"],
-  },
-  {
-    question: "What syntax do developers use to denote an array?",
-    choices: ["(x,y)", "[x,y]", "{x,y}", "None of the above"],
-  },
-  {
-    question: "Which of the following are Javascript data types?",
-    choices: ["String", "Boolean", "Objects", "All of the above"],
-  },
-  {
-    question: "What does console.log() do",
-    choices: ["Outputs a message to the web console", "Deletes a function", "Creates an element", "Creates a variable"],
-  },
-];
