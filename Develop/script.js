c3ee82690ea31ad938da266348e23613a6514c10
Jake McMillan <jakewmcmillan@gmail.com>
2022-07-26 20:43:47 -0400

    Changing my variable objects to arrays

--- Develop/script.js
+++ Develop/script.js
@@ -26,6 +26,14 @@ var quiz = [
       choices: ["Outputs a message to the web console", "Deletes a function", "Creates an element", "Creates a variable"],
     },
   ];
+
+var questionPrompts = ["Which of the following is a programming language?", "What 2 primary values does a Boolean represent?","What syntax do developers use to denote an array?","Which of the following are Javascript data types?", "What does console.log() do?",];
+var choices1 = ["Boolean", "console.log", "Javascript", "Array"];
+var choices2 = ["Top/Bottom", "Right/Left", "Even/Odd", "True/False"];
+var choices3 = ["(x,y)", "[x,y]", "{x,y}", "None of the above"];
+var choices4 = ["String", "Boolean", "Objects", "All of the above"];
+var choices5 = ["Outputs a message to the web console", "Deletes a function", "Creates an element", "Creates a variable"];
+
   
   console.log(quiz);
 
@@ -40,6 +48,16 @@ function timer() {
     }
   }, 1000);
 }
+
+function setQuizText() {
+    question.textContent = quiz.question[i];
+    question++;
+}
+option.addEventListener("click", function () {
+    option.textContent = choices2;
+    choices++;
+    setQuizText();
+})
 
 timer();
 
